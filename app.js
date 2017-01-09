e3299c0d0aa3515db8862260de7868a546eed28a
Shav <shavenchew@yahoo.com>
2017-01-09 12:52:23 -0800

    Twitter search keyword is working (limiting to 10 for now)

--- app.js
+++ app.js
@@ -7,7 +7,27 @@ var express  = require('express'),
 
 console.log(process.env.hello);
 
+var client = new Twitter({
+  consumer_key: process.env.twitterKey,
+  consumer_secret: process.env.twitterSecret,
+  access_token_key: process.env.twitterToken,
+  access_token_secret: process.env.twitterTokenSecret
+});
 
-app.listen(6000, function(){
+app.get('/', function(req, res){
+  client.get('https://api.twitter.com/1.1/search/tweets.json?q=macbook&count=10', function(error, tweets, response) {
+  if(error) console.log(error);
+  //console.log(tweets);  // The
+  tweets.statuses.forEach(function(tweet){
+    console.log(tweet.text);
+    console.log('By: ' + tweet.user.name);
+  })
+  //console.log(response);  // Raw response object.
+  res.send(tweets.statuses);
+});
+});
+
+
+app.listen('5000', function(){
   console.log('Running');
 });
